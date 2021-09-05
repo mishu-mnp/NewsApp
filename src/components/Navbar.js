229f8931560c6b286ac980827e8f4aca132c28db
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-05 16:07:26 +0530

    Tut27 Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">Coding News</a>
+                        <a className="navbar-brand" href="/">Coders News</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
--- src/components/News.js
+++ src/components/News.js
@@ -2,53 +2,40 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles = [
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
 
     constructor() {
         super();
-        console.log("I am a constructor of News Component")
+        // console.log("I am a constructor of News Component")
         this.state = {
-            articles: this.articles,
+            articles: [],
             loading: false
         }
     }
 
+    async componentDidMount() {
+        console.log('cdm')
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e1841b7b047944d1b5462e3cb101e664";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({articles: parsedData.articles})
+    }
+
     render() {
         return (
-            <>
+            
                 <div className="container">
-                    <h2 className=" my-3 mb-3">Top Headlines - Cricket India Vs Pakistan</h2>
+                    <h2 className=" my-3 mb-3">Top Headlines - Aaj ki Taaza Khabar Sunn lo</h2>
                     <div className="row">
                     {this.state.articles.map((element)=>{
                         return <div className="col-md-4 mb-4" key={element.url}>
-                            <NewsItem title={element.title.slice(0,40)} description={element.description.slice(0,85)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:"https://c.ndtvimg.com/2021-08/cglc0vfo_bhupesh-baghel_650x400_27_August_21.jpg"} newsUrl={element.url}/>
                         </div>  
                     })}
                                     
                     </div>
                 </div>
-            </>
+            
         )
     }
 }
