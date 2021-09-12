5b307ff5321c123ed18f0721cf5794fc79d1fedf
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-12 15:09:14 +0530

    Tut34 and 35 Done

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import ScrollButton from './ScrollButton'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 export class News extends Component {
@@ -25,7 +27,7 @@ export class News extends Component {
         super(props);
         this.state = {
             articles: [],
-            loading: false,
+            loading: true,
             page: 1,
             category: this.props.category,
             totalResults: 0
@@ -35,7 +37,7 @@ export class News extends Component {
 
     async updateNews() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-        this.setState({ loading: true });
+        // this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json()
 
@@ -56,6 +58,7 @@ export class News extends Component {
         this.updateNews()
     }
 
+
     handlePrevClick = async () => {
         this.setState({ page: this.state.page - 1 })
         this.updateNews()
@@ -69,32 +72,54 @@ export class News extends Component {
         }
     }
 
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 })
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+            page: this.state.page,
+            articles: this.state.articles.concat(parsedData.articles),
+            loading: false,
+            totalResults: parsedData.totalResults,
+        })
+    }
 
 
     render() {
 
         return (
 
-            <div className="container">
+            <>
+
                 <div className="header" style={{ margin: '2.5rem 0rem' }}>
                     <h2 className=" my-3 mb-3 text-center">Coders News - Top {this.capitalize(this.state.category)} Headlines</h2>
                 </div>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4 mb-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title.slice(0, 40) : "No Title"} description={element.description ? element.description.slice(0, 80) : "No Description"} imageUrl={element.urlToImage ? element.urlToImage : "https://c.ndtvimg.com/2021-08/cglc0vfo_bhupesh-baghel_650x400_27_August_21.jpg"} newsUrl={element.url} author={element.author ? element.author : "Unknown"} publishDate={element.publishedAt} source={element.source.name} badgeColor={this.props.badgeColor} />
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}>
+                    <div className="container">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4 mb-4" key={element.url} style={{ paddingTop: '12px' }}>
+                                    <NewsItem title={element.title ? element.title.slice(0, 40) : "No Title"} description={element.description ? element.description.slice(0, 80) : "No Description"} imageUrl={element.urlToImage ? element.urlToImage : "https://c.ndtvimg.com/2021-08/cglc0vfo_bhupesh-baghel_650x400_27_August_21.jpg"} newsUrl={element.url} author={element.author ? element.author : "Unknown"} publishDate={element.publishedAt} source={element.source.name} badgeColor={this.props.badgeColor} />
+                                </div>
+                            })}
                         </div>
-                    })}
+                    </div>
+                </InfiniteScroll>
 
-                </div>
-                <div className="container d-flex justify-content-between mb-5">
+
+                {/* <div className="container d-flex justify-content-between mb-5">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
                     <button type="button" className='btn btn-dark'>{this.state.page}</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-            </div>
-
+                </div> */}
+                <ScrollButton />
+            </>
         )
     }
 }
---
+++ src/components/ScrollButton.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+
+const ScrollButton = () => {
+
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true)
+        }
+        else if (scrolled <= 300) {
+            setVisible(false)
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    window.addEventListener('scroll', toggleVisible);
+
+    return (
+        <>
+            <button type="button" class="btn btn-dark" onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }}>ScrollToTop</button>
+        </>
+    );
+}
+
+export default ScrollButton;
